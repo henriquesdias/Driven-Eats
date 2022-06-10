e30b73242a63d1f857dd4d4f90173d0925f8bb5b
henriquesdias <henrique.dias1915@gmail.com>
2022-06-10 03:18:56 -0300

    unmark an element that was marked and fix the width of principal button

--- scripts.js
+++ scripts.js
@@ -2,51 +2,87 @@ const icon = document.querySelectorAll('.icon');
 const food = document.querySelectorAll('.first-row')
 const drink = document.querySelectorAll('.second-row')
 const dessert = document.querySelectorAll('.third-row')
-const button  =document.querySelector('.button');
+const button  = document.querySelector('.button');
 const options = document.querySelectorAll('.option');
 
 button.disabled = true;
 
 let count = 0;
 
+
 function selectedFood(iten , num) {
-    for (let i = 0 ; i < food.length ; i++) {
-        food[i].classList.remove('select-item');
-        icon[i].classList.remove('show-icon');
+    if (iten.classList.contains('select-item') ) {
+        iten.classList.toggle('select-item');
+        icon[num].classList.toggle('show-icon');
+        count--;
+        // ------Aqui vc reseta a opção de prato escolhida
+    } else {
+        for (let i = 0 ; i < food.length ; i++) {
+            food[i].classList.remove('select-item');
+            icon[i].classList.remove('show-icon');
+          
+        }
+        count++;
+        // ------Aqui vc adiciona as informações do prato
+        iten.classList.toggle('select-item');
+        icon[num].classList.toggle('show-icon');
     }
-    iten.classList.toggle('select-item');
-    icon[num].classList.toggle('show-icon');
-    count++;
+    verificated();
 }
 
 function selectedDrink(iten, num) {
-    for (let i = 0 ; i < drink.length ; i++) {
-        drink[i].classList.remove('select-item');
-        icon[i + 5].classList.remove('show-icon');
+    if (iten.classList.contains('select-item') ) {
+        iten.classList.toggle('select-item');
+        icon[num].classList.toggle('show-icon');
+        count--;
+        // ------Aqui vc reseta a opção de prato escolhida
+    } else {
+        for (let i = 0 ; i < drink.length ; i++) {
+            drink[i].classList.remove('select-item');
+            icon[i + 5].classList.remove('show-icon');
+        }
+        count++;
+        // ------Aqui vc adiciona as informações do prato
+        iten.classList.toggle('select-item');
+        icon[num].classList.toggle('show-icon');
     }
-    iten.classList.toggle('select-item');
-    icon[num].classList.toggle('show-icon');
-    count++;
+    verificated();
 }
 function selectedDessert(iten, num) {
-    for (let i = 0 ; i < dessert.length ; i++) {
-        dessert[i].classList.remove('select-item');
-        icon[i + 9].classList.remove('show-icon');
+    if (iten.classList.contains('select-item') ) {
+        iten.classList.toggle('select-item');
+        icon[num].classList.toggle('show-icon');
+        count--;
+        // ------Aqui vc reseta a opção de prato escolhida
+    } else {
+        for (let i = 0 ; i < dessert.length ; i++) {
+            dessert[i].classList.remove('select-item');
+            icon[i + 9].classList.remove('show-icon');
+        }
+        count++;
+        // ------Aqui vc adiciona as informações do prato
+        iten.classList.toggle('select-item');
+        icon[num].classList.toggle('show-icon');
     }
-    iten.classList.toggle('select-item');
-    icon[num].classList.toggle('show-icon');
-    count++;
+    verificated();
 
-    if (count >= 3) {
+}
+function verificated() {
+    if (count === 3) {
         button.innerHTML = 'Fechar pedido';
         button.classList.add('buttonGreen');
         button.disabled = false;
+    } else {
+        button.innerHTML = 'Selecione os 3 itens para fechar o pedido';
+        button.classList.remove('buttonGreen');
+        button.disabled = true;
     }
-
 }
+
 let confirm = document.querySelector('.transparent');
 
 function confirmOrder() {
+    console.log(count);
     confirm.classList.remove('hide');
 }
 function cancelOrder() {
@@ -56,7 +92,11 @@ function cancelOrder() {
 function completeOrder() {
     let name = prompt('Qual é o seu nome ?');
     let address = prompt('Qual o seu endereço ?');
+    if (name == '' || address == '') {
+        alert('Por favor, preencha o seu nome/endereço')
+    } else {
     let textFinal =  encodeURIComponent(`Olá, gostaria de fazer o pedido:- Prato: Frango Yin Yang- Bebida: Coquinha Gelada- Sobremesa: PudimTotal: R$ 27.70- Nome: ${name}- Endereço: ${address}`);
     const url =  `https://wa.me/?text=${textFinal}`;
     open(url);
+    }
 }
